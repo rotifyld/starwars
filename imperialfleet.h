3e58a90ce33a1845a9255721bb11a6d85238045e
rotifyld <dawidborys98@gmail.com>
2018-11-28 17:28:52 +0100

    floty

--- imperialfleet.h
+++ imperialfleet.h
@@ -1,4 +1,56 @@
 #ifndef STARWARS_IMPERIALFLEET_H
 #define STARWARS_IMPERIALFLEET_H
 
+#include "rebelfleet.h"
+
+#include <cassert>
+#include <type_traits>
+
+template<typename U> // todo
+class ImperialStarship {
+    U shield;
+    U attackPower;
+
+public:
+    using valueType = U;
+
+    ImperialStarship(const U shield, const U attackPower)
+            : shield(shield), attackPower(attackPower) {
+    }
+
+    U getShield() const { return shield; }
+
+    U getAttackPower() const { return attackPower; }
+
+    void takeDamage(const U damage) {
+        if (damage > shield) {
+            shield = U{0};
+        } else {
+            shield -= damage;
+        }
+
+    }
+};
+
+template <typename I, typename R>
+void attack(I &imperialShip, R &rebelShip) {
+    rebelShip.takeDamage(imperialShip.getAttackPower());
+    imperialShip.takeDamage(rebelShip.getAttackPower());
+}
+
+template <typename I, typename R>
+void attack(I &imperialShip, Explorer<R> &rebelShip) {
+    rebelShip.takeDamage(imperialShip.getAttackPower());
+}
+
+template<typename U>
+using DeathStar = ImperialStarship<U>;
+
+template<typename U>
+using ImperialDestroyer = ImperialStarship<U>;
+
+template<typename U>
+using TIEFighter = ImperialStarship<U>;
+
+
 #endif //STARWARS_IMPERIALFLEET_H
--- rebelfleet.h
+++ rebelfleet.h
@@ -1,7 +1,58 @@
 #ifndef STARWARS_REBELFLEET_H
 #define STARWARS_REBELFLEET_H
 
+#include <cassert>
+#include <type_traits>
 
+template<typename U, int minSpeed, int maxSpeed, bool canAttack> // todo
+class RebelStarship {
+    U shield;
+    U speed;
+    U attackPower;
 
+    void assert_speed() const {
+        assert(speed >= minSpeed && speed <= maxSpeed);
+    }
+
+public:
+
+    using valueType = U;
+
+    template<bool b = !canAttack, typename = typename std::enable_if<b>::type>
+    RebelStarship(const U shield, const U speed)
+            : shield(shield), speed(speed) {
+        assert_speed();
+    }
+
+    template<bool b = canAttack, typename = typename std::enable_if<b>::type>
+    RebelStarship(const U shield, const U speed, const U attackPower)
+            : shield(shield), speed(speed), attackPower(attackPower) {
+        assert_speed();
+    }
+
+    U getShield() const { return shield; }
+
+    U getSpeed() const { return speed; }
+
+    template<bool b = canAttack, typename = typename std::enable_if<b>::type>
+    U getAttackPower() const { return attackPower; }
+
+    void takeDamage(const U damage) {
+        if (damage > shield) {
+            shield = U{0};
+        } else {
+            shield -= damage;
+        }
+    }
+};
+
+template<typename U>
+using Explorer = RebelStarship<U, 299796, 2997960, false>;
+
+template<typename U>
+using StarCruiser = RebelStarship<U, 99999, 299795, true>;
+
+template<typename U>
+using XWing = RebelStarship<U, 299796, 2997960, true>;
 
 #endif //STARWARS_REBELFLEET_H
